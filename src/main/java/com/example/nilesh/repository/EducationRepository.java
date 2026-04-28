6edb9c136dd2023798a82897da8cff03c2377126
NILESH KUMAR <140897234+rnileshk@users.noreply.github.com>
2026-04-28 17:47:29 +0530

    Create EducationRepository interface for JPA

---
+++ src/main/java/com/example/nilesh/repository/EducationRepository.java
@@ -0,0 +1,7 @@
+package com.example.nilesh.repository;
+
+import com.example.nilesh.entity.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationRepository extends JpaRepository<Education, Long> {
+}
