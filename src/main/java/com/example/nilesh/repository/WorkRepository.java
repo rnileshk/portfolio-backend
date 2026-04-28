83cdb93750923e2b50ebae0342ac4caf252e6ac9
NILESH KUMAR <140897234+rnileshk@users.noreply.github.com>
2026-04-28 17:45:31 +0530

    Create WorkRepository interface for Work entity

---
+++ src/main/java/com/example/nilesh/repository/WorkRepository.java
@@ -0,0 +1,7 @@
+package com.example.nilesh.repository;
+
+import com.example.nilesh.entity.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkRepository extends JpaRepository<Work, Long> {
+}
